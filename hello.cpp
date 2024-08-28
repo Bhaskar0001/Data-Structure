ae308626ac5991d4a327fea10d66fb1ec39d051e
Bhaskar001 <bhaskarjoshi900@gmail.com>
2024-08-29 03:28:54 +0530

    1

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"hello"<<"\n";
+   
+
+    int num=10;
+    int i,j;
+    for(i=0; i<num; i++){
+        cout<<"n";
+        
+    }
+    return 0;
+}
